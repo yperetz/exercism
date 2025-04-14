532da180536a533549b4dfea0aad752de681d252
yperetz <yperetz26@gmail.com>
2025-04-14 22:12:29 +0300

    add pascal triangle
    Signed-off-by: yperetz <yperetz26@gmail.com>

--- zig/pascals-triangle/pascals_triangle.zig
+++ zig/pascals-triangle/pascals_triangle.zig
@@ -2,7 +2,27 @@ const std = @import("std");
 const mem = std.mem;
 
 pub fn rows(allocator: mem.Allocator, count: usize) mem.Allocator.Error![][]u128 {
-    _ = allocator;
-    _ = count;
-    @compileError("please implement the rows function");
+    var ans = try allocator.alloc([]u128, count);
+    errdefer allocator.free(ans);
+    if (count == 0)
+        return ans;
+    ans[0] = try allocator.alloc(u128, 1);
+    ans[0][0] = 1;
+    for (1..count) |i| {
+        ans[i] = allocator.alloc(u128, i + 1) catch |err| {
+            for (0..i) |idx| {
+                allocator.free(ans[i - 1 - idx]); // reverse order
+            }
+
+            return err;
+        };
+
+        for (0..i + 1) |j| {
+            const leftparent = if (j == 0) 0 else ans[i - 1][j - 1];
+            const rightparent = if (j == i) 0 else ans[i - 1][j];
+            ans[i][j] = leftparent + rightparent;
+        }
+    }
+
+    return ans;
 }
